591fc652fa3d7707c29fa7042efe1e873bcf44c9
Sam <rizky.pratama@sicepat.com>
2022-11-24 09:37:05 +0700

    add message tempalte

--- index.js
+++ index.js
@@ -51,45 +51,53 @@ app.post('/webhook', async (req, res) => {
             const from = changes.value.messages[0].from;
             const msg_body = changes.value.messages[0].text.body;
             const msg_id = changes.value.messages[0].id;
-            const contact_name = changes.value.contacts[0].profile.name || 'stranger';
+            const contact_name =
+                changes.value.contacts[0].profile.name || 'stranger';
             const url = `https://graph.facebook.com/v15.0/${phone_id}/messages?access_token=${ACCESS_TOKEN}`;
 
-            console.log(phone_id, "*phone_id")
-            console.log(from, "*from")
-            console.log(msg_body, "*msg_body")
-            console.log(ACCESS_TOKEN, "access_token");
-            console.log('https://graph.facebook.com/v15.0/'+phone_id+'/messages?access_token='+ACCESS_TOKEN, "url");
+            console.log(phone_id, '*phone_id');
+            console.log(from, '*from');
+            console.log(msg_body, '*msg_body');
+            console.log(ACCESS_TOKEN, 'access_token');
+            console.log(
+                'https://graph.facebook.com/v15.0/' +
+                    phone_id +
+                    '/messages?access_token=' +
+                    ACCESS_TOKEN,
+                'url'
+            );
 
             // Mark as Read
             await axios({
                 method: 'POST',
-                url: 'https://graph.facebook.com/v15.0/'+phone_id+'/messages',
+                url:
+                    'https://graph.facebook.com/v15.0/' +
+                    phone_id +
+                    '/messages',
                 data: {
                     messaging_product: 'whatsapp',
-                    status: "read",
-                    message_id: msg_id
+                    status: 'read',
+                    message_id: msg_id,
                 },
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${ACCESS_TOKEN}`
-                }
+                    Authorization: `Bearer ${ACCESS_TOKEN}`,
+                },
             });
 
             // Reply the message
-            await axios({
-                method: 'POST',
-                url: url,
-                data: {
-                    messaging_product: 'whatsapp',
-                    to: from,
-                    text: {
-                        body: `Hi ${contact_name}, your message is ${msg_body}`,
-                    }
-                },
-                headers: {
-                    'Content-Type': 'application/json'
+            if (msg_body?.toLowerCase() === 'cek resi') {
+                const tokpedHalu = 10000;
+                const tokpedGokil = 50000;
+                const data = {
+                    tokpedHalu,
+                    tokpedGokil
                 }
-            });
+                const urlCheckAwb = `https://graph.facebook.com/v15.0/${phone_id}/messages`;
+                await checkAwb(ACCESS_TOKEN, from, urlCheckAwb, data)
+            } else {
+                await defaultReply(contact_name, msg_body, url, from);
+            }
 
             res.sendStatus(200);
         } else {
@@ -98,6 +106,70 @@ app.post('/webhook', async (req, res) => {
     }
 });
 
+async function defaultReply(contact_name, msg_body, url, from) {
+    try {
+        // Reply the message
+        await axios({
+            method: 'POST',
+            url: url,
+            data: {
+                messaging_product: 'whatsapp',
+                to: from,
+                text: {
+                    body: `Hi ${contact_name}, your message is ${msg_body}`,
+                },
+            },
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function checkAwb(ACCESS_TOKEN, from, url, data) {
+    try {
+        await axios({
+            method: 'POST',
+            url: url,
+            data: {
+                messaging_product: 'whatsapp',
+                recipient_type: 'individual',
+                to: from,
+                type: 'template',
+                template: {
+                    name: 'status_resi',
+                    language: {
+                        code: 'id',
+                    },
+                    components: [
+                        {
+                            type: 'body',
+                            parameters: [
+                                {
+                                    type: 'text',
+                                    text: `${data.tokpedHalu}`,
+                                },
+                                {
+                                    type: 'text',
+                                    text: `${data.tokpedGokil}`,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            },
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${ACCESS_TOKEN}`
+            },
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 app.get('/', (req, res) => {
-    res.status(200).send('welcome to whatsapp webhook cloud api!')
+    res.status(200).send('welcome to whatsapp webhook cloud api!');
 });
